56ac086faa7be82a4bbb22615b6d0f0667c303be
John Gamboa <jayjeex@gmail.com>
2021-10-05 02:15:26 +0800

    added some test in gameboardFactory.test.js

--- src/js/test/gameboardFactory.test.js
+++ src/js/test/gameboardFactory.test.js
@@ -8,12 +8,12 @@ describe('Gameboard factory', () => {
         gameboard.setShipCoords('battleship', 'A', 1);
     });
 
-    test('return the expected object from the factory', () => {
+    it('return the expected object from the factory', () => {
         expect(gameboard)
             .toEqual({
                 admiral: 'John',
                 setShipCoords: expect.any(Function),
-                getAllCoords: expect.any(Function), // might not be needed
+                getAllCoords: expect.any(Function), // might not be needed or use as private
                 receiveAttack: expect.any(Function),
                 getMissedAttacks: expect.any(Function),
                 isReady: expect.any(Function),
@@ -21,19 +21,55 @@ describe('Gameboard factory', () => {
             });
     });
 
-    test('returns a string type value if the attack hit a ship coords', () => {
-        gameboard.setShipCoords('battleship', 'A', 1);
-        expect()
-            .toBe('A1');
+    it('returns an array if it successfully set a ship coordinates', () => {
+        expect(gameboard.setShipCoords('battleship', 'B', 1))
+            .toEqual(['B1', 'B2', 'B3', 'B4']);
     });
 
-    test('returns a string type value if the attack hit a ship coords', () => {
+    it('returns an array of placed ship coordinates', () => {
+        expect(gameboard.getAllCoords())
+            .toEqual([
+                { name: 'battleship', coords: ['A1', 'A2', 'A3', 'A4'] },
+            ]);
+    });
+
+    it('returns a string type value if the attack hit a ship coordinates', () => {
         expect(gameboard.receiveAttack('A1'))
             .toBe('A1');
     });
 
-    test('returns false if the attack missed', () => {
+    it('returns false if the attack missed', () => {
         expect(gameboard.receiveAttack('B1'))
-            .toBeFalsy();
+            .toBe(false);
+    });
+
+    it('returns an array of missed attacks', () => {
+        gameboard.receiveAttack('B1');
+        expect(gameboard.getMissedAttacks())
+            .toEqual(['B1']);
+    });
+
+    it('returns false if not all ship has been placed', () => {
+        expect(gameboard.isReady())
+            .toBe(false);
+    });
+
+    it('returns true if all ship has been placed', () => {
+        gameboard.setShipCoords('carrier', 'B', 1);
+        gameboard.setShipCoords('submarine', 'C', 1);
+        gameboard.setShipCoords('destroyer', 'D', 1);
+        gameboard.setShipCoords('patrol boat', 'E', 1);
+        expect(gameboard.isReady())
+            .toBeTruthy();
+    });
+
+    it('returns false if not all ship has sunk', () => {
+        expect(gameboard.isDefeated())
+            .toBe(false);
+    });
+
+    it('returns true if all ship has sunk', () => {
+        expect(gameboard.isDefeated())
+            .toBeTruthy();
     });
 });
