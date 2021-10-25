7a8c74a16d83d5f220a6b35a0d34b106947bc5ac
John Gamboa <jayjeex@gmail.com>
2021-10-25 19:55:52 +0800

    added name form

--- src/js/static.js
+++ src/js/static.js
@@ -1,11 +1,36 @@
 import elementFactory from './factories/elementFactory';
 import appendTo from './appendTo';
 
-const createAppContainer = () => {
-    const app = elementFactory('div', 'app');
-    appendTo('body', app);
-};
-
 export default function setup() {
-    createAppContainer();
+    // set language
+    document.querySelector('html').setAttribute('lang', 'en');
+    // create app main container
+    appendTo('body', elementFactory('div', 'app', { id: 'app' }));
+    // create header and form
+    appendTo('#app', [
+        elementFactory('h1', 'header', [
+            { id: 'header' },
+            { innerHTML: 'battle&shy;ship' }]),
+        elementFactory('form', 'name-form', [
+            { id: 'nameForm' },
+            { novalidate: '' },
+        ]),
+    ]);
+    // create fieldset
+    appendTo('#nameForm', elementFactory('fieldset', 'name-fieldset',
+        { id: 'nameFieldset' }));
+    // create input for name
+    appendTo('#nameFieldset', [
+        elementFactory('legend', 'name-legend', { innerHTML: 'Name:' }),
+        elementFactory('input', 'name-input', [
+            { id: 'nameInput' },
+        ]),
+    ]);
+    // create hidden small tag for input error
+    appendTo('#nameFieldset', elementFactory('small', 'name-error', { id: 'nameError' }));
+    // create play button
+    appendTo('#nameFieldset', elementFactory('button', 'play-button', [
+        { id: 'playButton' },
+        { innerHTML: 'play' },
+    ]));
 }
